85135c8bc4792a3a70644692d724f3c235c8f3cd
krisfox <krisfox@gmail.com>
2013-12-19 18:01:09 -0800

    added engine config

--- lib/ng_graph_paper/engine.rb
+++ lib/ng_graph_paper/engine.rb
@@ -1,5 +1,7 @@
 module NgGraphPaper
   class Engine < ::Rails::Engine
-    
+    initializer "ng_graph_paper.assets.precompile" do |app|
+      app.config.assets.precompile += %w(ng_graph_paper.js)
+    end
   end
 end
